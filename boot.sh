137b588ff2369911c4bdcea2da9a805f0cbbd4cc
eina <eina@eina.cc>
2026-08-31 11:24:40 +0200

    actualitzar script

--- boot.sh
+++ boot.sh
@@ -2,7 +2,7 @@
 qemu-system-x86_64 \
 	-m 2048 \
 	-smp 2 \
-	-drive file=lfs-done.qcow2,media=disk,if=virtio \
+	-drive file=lfs-penguin.qcow2,media=disk,if=virtio \
 	-nic user,model=virtio \
 	-virtfs local,path=.,mount_tag=pingu,security_model=mapped,id=pingu \
 	-nographic \
