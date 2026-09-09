c1a348eedf5e2c98af40dc9421779f2497cf5a96
eina <eina@eina.cc>
2026-09-09 18:31:07 +0200

    NOPUCMES

--- assignment09/proc.c
+++ assignment09/proc.c
@@ -13,17 +13,24 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("ecoma-ba");
 MODULE_DESCRIPTION("Registers /proc/mymounts");
 
-static struct proc_dir_entry *proc;
-
 // https://pointer-overloading.blogspot.com/2013/09/linux-creating-entry-in-proc-file.html ffs
 
 static int proc_show(struct seq_file *m, void *v)
 {
+	static char path_buff[PATH_MAX];
+	static char *path_ret;
+	struct path p;
+
 	struct mount *mnt, *tmp;
 	struct rb_root root = current->nsproxy->mnt_ns->mounts;
 
 	rbtree_postorder_for_each_entry_safe(mnt, tmp, &root, mnt_node) {
-		seq_printf(m, "%s\n", mnt->mnt.mnt_sb->s_id);
+		path_ret = NULL;
+		p.mnt = &mnt->mnt;
+		p.dentry = mnt->mnt.mnt_root;
+		path_ret = d_path(&p, path_buff, PATH_MAX);
+		if (path_ret)
+			seq_printf(m, "%-40s %s\n", mnt->mnt.mnt_sb->s_id, path_ret);
 	}
 	return 0;
 }
@@ -42,7 +49,7 @@ static const struct proc_ops pops = {
 
 static int __init hello(void)
 {
-        proc = proc_create("mymounts", 0444, NULL, &pops);
+        proc_create("mymounts", 0444, NULL, &pops);
 	return 0;
 }
 
